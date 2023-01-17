9ebefefbe870523f7182adf603386f035c2704a7
nirav <nirav.t.prajapati21@gmail.com>
2023-01-17 11:39:09 +0530

    Register service provider dynamically. Override laravel TranslationServiceProvider.

--- src/TranslationServiceProvider.php
+++ src/TranslationServiceProvider.php
@@ -15,34 +15,36 @@ class TranslationServiceProvider extends BaseTranslationServiceProvider
      */
     public function register()
     {
-        $this->app->singleton('translation-organizer', function ($app) {
-            $manager = $app->make('Pinetcodev\LaravelTranslationOrganizer\Services\Manager');
+        if (config('translation-organizer.enabled')) {
+            $this->app->singleton('translation-organizer', function ($app) {
+                $manager = $app->make('Pinetcodev\LaravelTranslationOrganizer\Services\Manager');
 
-            return $manager;
-        });
+                return $manager;
+            });
 
-        $this->app->singleton('translation.loader', function ($app) {
-            return new TranslationLoader($app['files'], $app['path.lang']);
-        });
+            $this->app->singleton('translation.loader', function ($app) {
+                return new TranslationLoader($app['files'], $app['path.lang']);
+            });
 
-        // $this->registerLoader();
-        $this->app->singleton('translator', function ($app) {
-            $loader = $app['translation.loader'];
+            // $this->registerLoader();
+            $this->app->singleton('translator', function ($app) {
+                $loader = $app['translation.loader'];
 
-            // When registering the translator component, we'll need to set the default
-            // locale as well as the fallback locale. So, we'll grab the application
-            // configuration so we can easily get both of these values from there.
-            $locale = $app['config']['app.locale'];
+                // When registering the translator component, we'll need to set the default
+                // locale as well as the fallback locale. So, we'll grab the application
+                // configuration so we can easily get both of these values from there.
+                $locale = $app['config']['app.locale'];
 
-            $trans = new Translator($loader, $locale);
+                $trans = new Translator($loader, $locale);
 
-            $trans->setFallback($app['config']['app.fallback_locale']);
+                $trans->setFallback($app['config']['app.fallback_locale']);
 
-            if ($app->bound('translation-organizer')) {
-                $trans->setTranslationManager($app['translation-organizer']);
-            }
+                if ($app->bound('translation-organizer')) {
+                    $trans->setTranslationManager($app['translation-organizer']);
+                }
 
-            return $trans;
-        });
+                return $trans;
+            });
+        }
     }
 }
