fcfbd137de4b9a90d12a12e0d1e12698509d3522
nirav <nirav.t.prajapati21@gmail.com>
2023-04-18 15:04:50 +0530

    Error message while translation is enabled and try to import.

--- src/Services/Manager.php
+++ src/Services/Manager.php
@@ -635,9 +635,9 @@ class Manager
     protected function storeData()
     {
         $config = $this->app['config'];
-        if ($config->get('translation-organizer.enabled')) {
+        //if ($config->get('translation-organizer.enabled')) {
             $driver = $config->get('translation-organizer.storage.driver', 'file');
             cache()->driver($driver)->set($this->requestId, $this->data);
-        }
+//        }
     }
 }
