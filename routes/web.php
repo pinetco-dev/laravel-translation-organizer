1c72bd619b609f0c8901a378cc947836d6f58637
nirav <nirav.t.prajapati21@gmail.com>
2023-06-09 13:00:16 +0530

    route access for frontend translation.

--- routes/web.php
+++ routes/web.php
@@ -5,15 +5,14 @@ use Pinetcodev\LaravelTranslationOrganizer\Http\Controllers\TranslationControlle
 
 Route::middleware(config('translation-organizer.middleware_admin'))->group(function () {
     Route::get('/', [TranslationController::class, 'index'])->name('translation_organizer.index');
-
     Route::prefix('phrases')->group(function () {
         Route::get('/edit/{translation}', [TranslationController::class, 'phrase'])->name('translation_organizer.show');
     });
-    Route::post('/', [TranslationController::class, 'store'])->name('translation_organizer.store');
     Route::get('fetch/{id}', [TranslationController::class, 'fetch'])->name('translation_organizer.fetch');
 });
 
 Route::middleware(config('translation-organizer.middleware'))->group(function () {
     Route::post('toggle-translations', [TranslationController::class, 'toggle'])->name('translation_organizer.toggle');
     Route::any('toggle-translations-enable', [TranslationController::class, 'toggleEnable'])->name('translation_organizer.toggle-enable');
+    Route::post('/', [TranslationController::class, 'store'])->name('translation_organizer.store');
 });
