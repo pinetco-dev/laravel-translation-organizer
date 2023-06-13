562b8d3d43b38d91345ac1d91d28cec0c7685c0c
nirav <nirav.t.prajapati21@gmail.com>
2023-06-13 11:59:18 +0530

    change admin middleware

--- routes/web.php
+++ routes/web.php
@@ -8,10 +8,10 @@ Route::middleware(config('translation-organizer.middleware_admin'))->group(funct
     Route::prefix('phrases')->group(function () {
         Route::get('/edit/{translation}', [TranslationController::class, 'phrase'])->name('translation_organizer.show');
     });
-    Route::get('fetch/{id}', [TranslationController::class, 'fetch'])->name('translation_organizer.fetch');
 });
 
 Route::middleware(config('translation-organizer.middleware'))->group(function () {
+    Route::get('fetch/{id}', [TranslationController::class, 'fetch'])->name('translation_organizer.fetch');
     Route::post('toggle-translations', [TranslationController::class, 'toggle'])->name('translation_organizer.toggle');
     Route::any('toggle-translations-enable', [TranslationController::class, 'toggleEnable'])->name('translation_organizer.toggle-enable');
     Route::post('/', [TranslationController::class, 'store'])->name('translation_organizer.store');
